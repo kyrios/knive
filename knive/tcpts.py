e08d7e3c028f097d9e2faa0cfaef3fb63fc13525
Thorsten Philipp <github@kyri0s.de>
2012-03-11 20:56:12 +0100

    Added TCPTSClient

--- knive/tcpts.py
+++ knive/tcpts.py
@@ -1,25 +1,161 @@
+#
 # tcpts.py
 # Copyright (c) 2012 Thorsten Philipp <kyrios@kyri0s.de>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of
+# this software and associated documentation files (the "Software"), to deal in the 
+# Software without restriction, including without limitation the rights to use, copy,
+# modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, 
+# and to permit persons to whom the Software is furnished to do so, subject to the
+# following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+# INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+# PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+# HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION 
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+# SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#
 
-# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+from foundation import KNOutlet, KNInlet
 
-# The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-from foundation import KNDistributor
-
-from twisted.internet.protocol      import ReconnectingClientFactory, ClientFactory, Protocol, ServerFactory
+from twisted.internet.protocol      import ReconnectingClientFactory, ServerFactory
 from twisted.protocols              import basic
-from twisted.application            import service, internet
+from twisted.application            import internet
 from zope.interface                 import implements
 from twisted.python                 import log
-from twisted.internet               import reactor, defer
+from twisted.internet               import reactor
 from twisted.internet.interfaces    import ILoggingContext
+from twisted.internet.defer         import Deferred 
 
 import hashlib
 import random
-import datetime
-import logging
+
+class TCPTSClientFactory(ReconnectingClientFactory):
+    """docstring for TCPTransportFactory"""
+    
+    initialDelay = .1
+    secret = None
+    maxDelay = 8
+
+    def clientConnectionFailed(self, connector, reason):
+        log.err('connection failed: %s' % reason)
+        if self.continueTrying:
+            self.connector = connector
+            self.retry()
+    
+    def clientConnectionLost(self, connector, reason):
+        log.err('connection lost: %s' % reason)
+        if self.continueTrying:
+            self.connector = connector
+            self.retry()
+            
+    def buildProtocol(self, addr):
+        log.msg("TCPTSClient connected to %s" % addr)
+        self.protocol = TCPTSClientProtocol()
+        self.protocol.factory = self
+        return self.protocol
+                
+        
+class TCPTSClient(KNOutlet):
+    """Connect to a simple TCP Server accepting Mpeg-TS Data after a simple handshake auth"""
+    implements(ILoggingContext)
+    
+    
+    def __init__(self, hostname,port,secret='12345'):
+        super(TCPTSClient,self).__init__(name="%s:%s" % (hostname,port))
+        self.hostname = hostname
+        self.port = port
+        self.factory = TCPTSClientFactory()        
+        self.factory.service = self
+        self.factory.secret = secret
+        self.connection = internet.TCPClient(self.hostname, self.port, self.factory)
+        self.canSendData = False
+        
+    def logPrefix(self):
+        """docstring for logPrefix"""
+        return 'TCPTSClient'
+    
+    def _start(self):
+        """docstring for startService"""
+        self.connection.startService()
+        defer = Deferred()
+        
+        def _checkRunning():
+            if not self.factory:
+                self.log.debug('Factory not ready')
+            if not self.factory.protocol:
+                self.log.debug('Protocol not ready')
+            else:
+                if self.factory.protocol.state == 99:
+                    self.log.info('Connection established')
+                    self.canSendData = True
+                    defer.callback(self)
+                    return
+                else:
+                    self.log.debug('Unknown protocol state %s' % self.factory.protocol.state)
+            self.log.info("Waiting for connecion to %s:%s" % (self.hostname,self.port))
+            reactor.callLater(1,_checkRunning)
+                
+        _checkRunning()
+        return defer
+                
+    def dataReceived(self,data):
+        """docstring for dataReceived"""
+        if self.canSendData:
+            self.factory.protocol.transport.write(data)
+        else:
+            log.err("Service not running yet. Can't send data")
+        
+    def connectionFailed(self):
+        """docstring for connectionFailed"""
+        reactor.stop()
+        
+    def __str__(self):
+        """docstring for __str__"""
+        return "%s ->%s:%s" % (self.__class__.__name__,self.hostname,self.port)
+
+class TCPTSClientProtocol(basic.LineReceiver):
+    """docstring for TCPTSClientProtocol"""
+    challenge = None
+    
+    def connectionMade(self):
+        self.state = 0
+        self.challenge = None
+        log.msg("Connected TCPTSClientProto")
+    
+    def lineReceived(self,line):
+        """docstring for lineReceived"""
+        #log.msg("Received %s" % line)
+        if self.state == 0:
+            if line == 'TCPTS 0.1':
+                self.state = 1
+                log.msg("Connected to TCPTSServer")
+            else:
+                self.protocolMissmatch()
+        elif self.state == 1:
+            log.msg("Secret: %s" % self.factory.secret)
+            log.msg("Sending challenge reply: %s - %s - %s" % (line,self.factory.secret,hashlib.sha224(line + self.factory.secret).hexdigest()))
+            self.sendLine(hashlib.sha224(line + self.factory.secret).hexdigest())
+            self.state = 2
+        elif self.state == 2:
+            if line == 'Authenticated':
+                log.msg("Autenticated")
+                self.state = 99
+            else:
+                log.err("Authentication not succesfull. Check secret on client/server")
+                reactor.stop()
+        else:
+            log.err("Unknown data received from server")
+            self.transport.loseConnection()
+    
+    def protocolMissmatch(self):
+        """docstring for protocolMissmatch"""
+        log.err("Protocol missmatch")
+        self.transport.loseConnection()
 
 
 class TCPTSServerProtocol(basic.LineReceiver):
@@ -66,7 +202,7 @@ class TCPTSServerProtocol(basic.LineReceiver):
         """handle the mpeg-ts data"""
         self.factory.service.dataReceived(data)
 
-class TCPTSServer(KNDistributor):
+class TCPTSServer(KNInlet):
     """Create a simple TCP Server accepting Mpeg-TS Data after a handshake auth"""
 
     def __init__(self, name='Unknown',hostname="0.0.0.0",port=3333,secret='123467'):
@@ -82,8 +218,11 @@ class TCPTSServer(KNDistributor):
         self.log.err('Connection failed. Can not continue.')
         reactor.stop()
 
-    def _didStart(self):
+    def outletStarted(self,outlet):
         """Stuff to be done after all outlets have started but before the inlet is notified"""
+        self.log.info("Starting")
+
+    def _start(self):
         self.connection.startService()
 
     def _willStop(self):
